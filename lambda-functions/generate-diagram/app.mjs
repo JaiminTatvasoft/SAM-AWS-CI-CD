74936c325e2559128cf14a38ec6d4663ff88b20f
JaiminTatvasoft <jaiminkumar.bhojani@tatvasoft.com>
2025-07-23 16:25:58 +0530

    secure the API key in yaml file

--- lambda-functions/generate-diagram/app.mjs
+++ lambda-functions/generate-diagram/app.mjs
@@ -1,23 +1,26 @@
-import axios from "axios";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 export const generateDiagramHandler = async (event) => {
   let prompt = JSON.parse(event.body).prompt;
   console.log("Prompt received:", prompt);
 
-  const GEMINI_API_KEY = "AIzaSyCpufyUUpFtn1OjQNEtfvPHJCWqizTuEWE";
-  const GEMINI_ENDPOINT = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCpufyUUpFtn1OjQNEtfvPHJCWqizTuEWE`;
-  const payload = {
-    contents: [{ parts: [{ text: prompt }] }],
-  };
+  const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+  if (!GEMINI_API_KEY) {
+    console.error("Missing GEMINI_API_KEY!");
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Server misconfiguration" }),
+    };
+  }
 
-  const resp = await axios.post(GEMINI_ENDPOINT, payload, {
-    headers: { "Content-Type": "application/json" },
-    timeout: 10000,
+  const llm = new ChatGoogleGenerativeAI({
+    model: "gemini-2.0-flash",
+    temperature: 1,
+    apiKey: GEMINI_API_KEY,
   });
 
-  const jsonResponse = resp.data.candidates[0].content.parts[0].text;
-
-  console.log("Gemini response status:", jsonResponse);
+  const result = await llm.invoke(prompt);
+  const jsonResponse = result.content;
 
   const response = {
     statusCode: 200,
