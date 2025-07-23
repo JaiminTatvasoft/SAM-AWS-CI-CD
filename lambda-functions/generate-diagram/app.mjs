05d8de88ce8995626692d6fde7b59d402b8cbe98
JaiminTatvasoft <jaiminkumar.bhojani@tatvasoft.com>
2025-07-23 14:22:16 +0530

    Issues in generate diagram

--- lambda-functions/generate-diagram/app.mjs
+++ lambda-functions/generate-diagram/app.mjs
@@ -1,28 +1,29 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import axios from "axios";
 
 export const generateDiagramHandler = async (event) => {
-  //   let prompt = JSON.parse(event.body).prompt;
-  let prompt = event.prompt || "Generate a diagram based on the provided data.";
+  let prompt = JSON.parse(event.body).prompt;
+  //   let prompt = event.prompt || "Generate a diagram based on the provided data.";
   console.log("Prompt received:", prompt);
 
-  //   const llm = new ChatGoogleGenerativeAI({
-  //     model: "gemini-2.0-flash",
-  //     temperature: 1,
-  //     apiKey: "AIzaSyA-iKZ8zrrWzZCnIupptSK1Oy6U2L5cYmo",
-  //   });
-
-  //   const response = await llm.invoke(prompt);
-  //   console.log("LLM response:", response.content);
-
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify({
-  //       message: "Diagram Generated!",
-  //       //   promptResponse: response.content,
-  //     }),
-  //   };
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "User processed!" }),
+  const GEMINI_API_KEY = "AIzaSyA-iKZ8zrrWzZCnIupptSK1Oy6U2L5cYmo";
+  const GEMINI_ENDPOINT = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
+  const payload = {
+    contents: [{ parts: [{ text: "Hello, can you generate diagram?" }] }],
   };
+
+  const resp = await axios.post(GEMINI_ENDPOINT, payload, {
+    headers: { "Content-Type": "application/json" },
+    timeout: 10000,
+  });
+
+  console.log("Gemini response status:", resp);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Diagram generated successfully!",
+    }),
+  };
+
+  return response;
 };
